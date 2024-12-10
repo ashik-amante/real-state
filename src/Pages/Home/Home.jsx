e4db1d7cab18d3eae19985a399a07d5db2edf6a9
Ashik <abdullahashik17@gmail.com>
2024-12-10 21:13:35 +0600

    setup,navbar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navber from "../Shared/Navber/Navber";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navber></Navber>
+            Home
+        </div>
+    );
+};
+
+export default Home;
